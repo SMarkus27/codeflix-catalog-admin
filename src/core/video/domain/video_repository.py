e194214ff9c2ddac21d3099efc22e000c5c48286
SMarkus27 <pereira.markus.27@hotmail.com>
2025-03-11 18:02:44 -0300

    feat: video repository

---
+++ src/core/video/domain/video_repository.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+from uuid import UUID
+
+from src.core.video.domain.video import Video
+
+
+class VideoRepository(ABC):
+
+    @abstractmethod
+    def save(self, genre: Video) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_by_id(self, id: UUID) -> Video | None:
+        raise NotImplementedError
+
+    @abstractmethod
+    def list(self) -> list[Video]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def delete(self, id: UUID) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    def update(self, genre) -> None:
+        raise NotImplementedError
